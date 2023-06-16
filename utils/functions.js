fda0654a2e4e39fbcfce07696696b861448dae68
aamir12 <aamirkhan8878@gmail.com>
2023-06-16 15:06:12 +0530

    upate

--- utils/functions.js
+++ utils/functions.js
@@ -1,5 +1,6 @@
 const getIp = (req) => {
   let ip;
+  console.log(JSON.stringify(req, null, 2));
   if (req.headers["x-forwarded-for"]) {
     ip = req.headers["x-forwarded-for"].split(",").pop().trim();
   } else if (req.connection && req.connection.remoteAddress) {
